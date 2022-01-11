20a8d59a9379dbc35c7f8fa1af833a87da95cb7a
Bunga Islamiya Putri <97377281+bungaislamiya@users.noreply.github.com>
2022-01-11 17:16:44 +0700

    Point2D

---
+++ point2d.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author lenovo
+ */
+public class point2d {
+    
+    public float x;
+    public float y;
+    private float k=1;
+    
+    public point2d (float in_x, float in_y) {
+        x = in_x;
+        y = in_y;
+    }//end constructor
+    
+    public float x() {
+        return (x);
+    }
+    
+    public float y() {
+        return (y);
+    }
+    
+}//end class point2d
